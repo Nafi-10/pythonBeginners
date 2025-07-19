d50a200b8ec0728ab067c8fdf94ae70b0af978cb
Kazi Jawad <Kazijawadulislam@gmail.com>
2025-07-20 00:40:17 +0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,7 @@
+name = "John"
+age = 70
+
+print("There once was a man named " + name)
+print("He was " + str(age) + " years old")
+print("He really liked the name " + name)
+print("But didn't like being " + str(age) + " years old")
